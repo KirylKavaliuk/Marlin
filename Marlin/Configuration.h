8dd8337ffefb700a6b517af5f97975399bf66302
KirylKavaliuk <kiryl_kavaliuk@epam.com>
2020-06-06 15:57:16 +0300

    set up z safe homing pos

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1362,8 +1362,8 @@
 #define Z_SAFE_HOMING
 
 #if ENABLED(Z_SAFE_HOMING)
-  #define Z_SAFE_HOMING_X_POINT ((X_BED_SIZE) / 2)    // X point for Z homing when homing all axes (G28).
-  #define Z_SAFE_HOMING_Y_POINT ((Y_BED_SIZE) / 2)    // Y point for Z homing when homing all axes (G28).
+  #define Z_SAFE_HOMING_X_POINT (X_OFFSET + (X_BED_SIZE) / 2)    // X point for Z homing when homing all axes (G28).
+  #define Z_SAFE_HOMING_Y_POINT (Y_OFFSET + (Y_BED_SIZE) / 2)    // Y point for Z homing when homing all axes (G28).
 #endif
 
 // Homing speeds (mm/m)
